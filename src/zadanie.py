031fa7e9cdc301f3c73d5ead8231bbc42c5948e0
xpartla <xpartla@stuba.sk>
2023-09-27 23:07:02 +0200

    gpt hellhole

--- src/zadanie.py
+++ src/zadanie.py
@@ -1,3 +1,6 @@
+from queue import Queue
+import copy
+
 
 class Car:
     def __init__(self, color, size, pos_x, pos_y, orientation, name):
@@ -11,34 +14,30 @@ class Car:
     def UP(self):
         if self.orientation == "v" and self.pos_y > 1:
             self.pos_y -= 1
-        elif self.orientation == "h":
-            print("Wrong car orientation")
-        elif self.pos_y <= 1:
-            print("Cannot move further up :(")
 
     def DOWN(self):
         if self.orientation == "v" and self.pos_y < 6:
             self.pos_y += 1
-        elif self.orientation == "h":
-            print("Wrong car orientation")
-        elif self.pos_y >= 6:
-            print("Cannot move further down :(")
 
     def RIGHT(self):
         if self.orientation == "h" and self.pos_x < 6:
             self.pos_x += 1
-        elif self.orientation == "v":
-            print("Wrong car orientation")
-        elif self.pos_x >= 6:
-            print("Cannot move further right :(")
 
     def LEFT(self):
         if self.orientation == "h" and self.pos_x > 1:
             self.pos_x -= 1
-        elif self.orientation == "v":
-            print("Wrong car orientation")
-        elif self.pos_x <= 1:
-            print("Cannot move further left :(")
+
+
+def end_state_check(cars):
+    # Check if the red car is at the exit position (5, 3)
+    for car in cars:
+        if car.name == "R" and car.pos_x == 5 and car.pos_y == 3:
+            return True
+    return False
+
+
+def state_tuple(cars):
+    return tuple((car.color, car.size, car.pos_x, car.pos_y, car.orientation, car.name) for car in cars)
 
 
 def printout(matrix):
@@ -47,12 +46,10 @@ def printout(matrix):
 
 
 def update_matrix(matrix, cars):
-    # Clear the entire matrix
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             matrix[i][j] = "X"
 
-    # Update the matrix with the new car positions
     for car in cars:
         if car.orientation == "h":
             for i in range(car.size):
@@ -60,6 +57,60 @@ def update_matrix(matrix, cars):
         else:
             for i in range(car.size):
                 matrix[car.pos_y + i - 1][car.pos_x - 1] = car.name
+
+
+def get_next_states(cars):
+    next_states = []
+    for car in cars:
+        if car.orientation == "h":
+            if car.pos_x > 1 and all(
+                    car.pos_x - 1 != c.pos_x or c.orientation == "v" or c.pos_y > car.pos_y + car.size or c.pos_y + c.size < car.pos_y
+                    for c in cars):
+                new_cars = copy.deepcopy(cars)
+                new_cars[cars.index(car)].LEFT()
+                next_states.append(new_cars)
+            if car.pos_x + car.size < 6 and all(
+                    car.pos_x + car.size != c.pos_x or c.orientation == "v" or c.pos_y > car.pos_y + car.size or c.pos_y + c.size < car.pos_y
+                    for c in cars):
+                new_cars = copy.deepcopy(cars)
+                new_cars[cars.index(car)].RIGHT()
+                next_states.append(new_cars)
+        else:
+            if car.pos_y > 1 and all(
+                    car.pos_y - 1 != c.pos_y or c.orientation == "h" or c.pos_x > car.pos_x + car.size or c.pos_x + c.size < car.pos_x
+                    for c in cars):
+                new_cars = copy.deepcopy(cars)
+                new_cars[cars.index(car)].UP()
+                next_states.append(new_cars)
+            if car.pos_y + car.size < 6 and all(
+                    car.pos_y + car.size != c.pos_y or c.orientation == "h" or c.pos_x > car.pos_x + car.size or c.pos_x + c.size < car.pos_x
+                    for c in cars):
+                new_cars = copy.deepcopy(cars)
+                new_cars[cars.index(car)].DOWN()
+                next_states.append(new_cars)
+    return next_states
+
+
+def bfs(initial_state):
+    visited_states = set()
+    queue = Queue()
+    queue.put((initial_state, []))
+
+    while not queue.empty():
+        curr_state, path = queue.get()
+        curr_state_tuple = state_tuple(curr_state)  # Convert to a tuple
+        if curr_state_tuple in visited_states:
+            continue
+        visited_states.add(curr_state_tuple)
+
+        if end_state_check(curr_state):
+            return path
+
+        for next_state in get_next_states(curr_state):
+            queue.put((next_state, path + [curr_state]))
+
+    # If no solution is found, return an empty list
+    return []
 
 
 c1 = Car("Red", 2, 2, 3, "h", "R")
@@ -76,57 +127,16 @@ Cars = [c1, c2, c3, c4, c5, c6, c7, c8]
 rows = 6
 cols = 6
 matrix = [["X" for _ in range(cols)] for _ in range(rows)]
-for car in Cars:
-    update_matrix(matrix, Cars)
+update_matrix(matrix, Cars)
 
+initial_state = Cars
+solution_path = bfs(initial_state)
 
-def input_handler():
+if solution_path:
+    print("Solution found!")
+    matrix = [["X" for _ in range(cols)] for _ in range(rows)]
+    update_matrix(matrix, solution_path[-1])  # Update with the last state in the path
     printout(matrix)
-    while True:
-        car_name = input("Enter the color of the car you want to move (R, O, "
-                         "W, B, Y, P, G, V) or press 'q' to QUIT : ")
-        if(car_name) == "q":
-            break
-        found = False
-        for car in Cars:
-            if car.name == car_name:
-                found = True
-                user_input = input(f"you cose the car {car_name} :"
-                                   "press 'w' to go UP,"
-                                   "press 'a' to go LEFT,"
-                                   "press 's' to go DOWN,"
-                                   "press 'd' to go RIGHT,"
-                                   "Press 'q' to QUIT : ")
-                if user_input == "w":
-                    car.UP()
-                    update_matrix(matrix, Cars)
-                    printout(matrix)
-                    break
+else:
+    print("No solution found.")
 
-                elif user_input == "s":
-                    car.DOWN()
-                    update_matrix(matrix, Cars)
-                    printout(matrix)
-                    break
-
-                elif user_input == "a":
-                    car.LEFT()
-                    update_matrix(matrix, Cars)
-                    printout(matrix)
-                    break
-
-                elif user_input == "d":
-                    car.RIGHT()
-                    update_matrix(matrix, Cars)
-                    printout(matrix)
-                    break
-
-                elif user_input == "q":
-                    break
-                else:
-                    print("Invalid input. use 'w - a - s - d - q' ")
-        if not found:
-            print(f"Car '{car_name}' not found. Enter correct name ")
-
-
-input_handler()
